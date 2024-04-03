9e4b587be4fcb0679d00bfb65425742de4b64ac4
AntonSadkov <antonsadkov96@gmail.com>
2024-04-03 22:31:53 +0300

    7.8 Конструктор.

---
+++ src/main/java/ru/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Наличие ошибки: " + active);
+        System.out.println("Код ошибки: " + status);
+        System.out.println("Сообщение об ошибке: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error errorServer = new Error(true, 500, "Ошибка сервера.");
+        error.printInfo();
+        errorServer.printInfo();
+    }
+}
